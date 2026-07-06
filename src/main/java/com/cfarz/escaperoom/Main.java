be0f942caa16625615e0e92572794881a2014b79
Christine Farrus Zaczyk <cfarzac@email.com>
2026-07-06 18:26:40 +0200

    Started the set up for the game

---
+++ src/main/java/com/cfarz/escaperoom/Main.java
@@ -0,0 +1,8 @@
+package com.cfarz.escaperoom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Escape Room!");
+        System.out.println("Will you be able to get out...");
+    }
+}
